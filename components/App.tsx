aaf34de1ab499ff0a5e967f6b9bf48a1fd1d27d3
MasterZensei <zhan.qureshi@gmail.com>
2025-05-09 01:20:57 +0300

    Initial modular refactor: entry point, App scaffold, Header component

---
+++ components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <h1 className="text-3xl font-bold text-blue-800">Welcome to the Procurement Platform</h1>
+      {/* TODO: Import and render main layout, routing, and pages here */}
+    </div>
+  );
+};
+
+export default App; 
